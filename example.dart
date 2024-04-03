ff5931588e9d532993221259ae63c2db6befb818
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2024-04-03 08:54:49 +0200

    add my new awesome feature

--- example.dart
+++ example.dart
@@ -1,5 +1,13 @@
 // entry point
 void main(List<String> args) {
+  final feature = MyNewFeature();
   // this prints hello world!
   print('hello world! args: $args');
+  feature.doSomething();
+}
+
+class MyNewFeature {
+  void doSomething() {
+    print('I did something awesome!');
+  }
 }
