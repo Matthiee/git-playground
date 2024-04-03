596a4ccae0a4af33ed7fec3b0add58133d55a5b8
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2024-04-03 10:49:58 +0200

    add second feature

--- example.dart
+++ example.dart
@@ -3,12 +3,20 @@ void main(List<String> args) {
   // This prints hello world!
   print('hello world! args: $args');
 
-  final feature = MyFirstFeature();
+  final feature = MySecondFeature();
   feature.doSomething();
 }
 
 class MyFirstFeature {
   void doSomething() {
     print("I'm the first feature");
+  }
+}
+
+class MySecondFeature extends MyFirstFeature {
+  @override
+  void doSomething() {
+    super.doSomething();
+    print("I'm the second feature");
   }
 }
