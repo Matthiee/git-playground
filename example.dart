ec3e8fe12787f284273946eb7184fd268a677d77
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2024-04-03 11:33:29 +0200

    add first feature (#3)

--- example.dart
+++ example.dart
@@ -2,4 +2,13 @@
 void main(List<String> args) {
   // This prints hello world!
   print('hello world! args: $args');
+
+  final feature = MyFirstFeature();
+  feature.doSomething();
+}
+
+class MyFirstFeature {
+  void doSomething() {
+    print("I'm the first feature");
+  }
 }
