a55ae706cd5bf4cbfab742b3e683bf54478bea81
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2024-04-08 08:36:44 +0200

    add my feature

--- example.dart
+++ example.dart
@@ -1,3 +1,11 @@
 void main(List<String> args) {
+  final feature = MyFeature();
   print('hello world');
+  feature.doSomething();
+}
+
+class MyFeature {
+  void doSomething() {
+    print('fix this');
+  }
 }
