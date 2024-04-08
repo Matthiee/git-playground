6a9aeafece5ada398d8f2853e8a75ff7dd7e05c2
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2024-04-08 08:36:44 +0200

    add my feature

--- example.dart
+++ example.dart
@@ -1,5 +1,13 @@
 // Entry point
 void main(List<String> args) {
+  final feature = MyFeature();
   // This prints hello world!
   print('hello world! args: $args');
+  feature.doSomething();
+}
+
+class MyFeature {
+  void doSomething() {
+    print('fixed');
+  }
 }
